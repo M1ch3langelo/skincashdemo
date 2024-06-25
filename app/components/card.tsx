d50bfa08f70a8b8e70b6ea9f6033e37fe541d600
Michelangelo <michelangelo.garces@hotmail.com>
2024-06-25 06:31:21 -0500

    updates

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -3,18 +3,19 @@
 import React from "react";
 import Image from "next/image";
 
-export default function HomeCardFN({ title, image}: { title: string, image: string }) {
+export default function HomeCardFN({ title }: { title: string,}) {
     return (
-        <div className="relative from-[#D72142] to-transparent border-b-2 border-b-[#D72142] border-[#3C3C3C] shadow-md p-4 scroll-ml-6 max-w-[150.45px] card border-[1px] cursor-pointer hover:scale-105 transition-all h-[212px] place-content-center">
-            <Image src="/images/cardimage.png" alt="AWP | Neo Noir" width={120} height={120} className="absolute" />
-            <Image src="/images/cardtopright.svg" alt="AWP | Neo Noir" width={15.24} height={15.36} className="absolute top-0 right-0 z-10" />
+        <div className="relative from-[#D72142] to-transparent border-b-2 border-b-[#D72142] shadow-md p-4 scroll-ml-6 max-w-[150.45px] card cursor-pointer hover:scale-105 transition-all h-[212px] place-content-center">
+            <Image src="/images/cardimage.png" alt="image" width={120} height={120} className="absolute top-5" />
+            <Image src="/images/cardtopright.svg" alt="topright" width={10.24} height={15.36} className="absolute top-0 right-0 z-10" />
+            <Image src="/images/cardbackground.png" alt="Lines" layout="fill" objectFit="cover" quality={100} className="absolute z-30" />
             <div className="flex flex-col items-center">
                 <div className="flex absolute top-2 left-1 items-center">
-                    <p className="bg-[#2D2D2D4D] rounded-full text-[#D72142CC] font-bold p-1">FN</p>
-                    <div className="ml-2 flex text-white font-bold text-md">50.89<p className="text-[#3FFE9E]">$</p></div>
+                    <p className="bg-[#2D2D2D4D] rounded-full text-[#D72142CC] font-bold p-1 text-[10px]">FN</p>
+                    <div className="ml-2 flex text-white font-bold text-[11px]">50.89<p className="text-[#3FFE9E]">$</p></div>
                 </div>
-                <img src={image} className="w-[120px]" alt={title} />
-                <p className="mt-2 font-bold text-white text-nowrap">AWP | Neo Noir</p>
+                <Image src="/images/awp.png" width={132.67} height={110.18} alt="alt" />
+                <p className="mt-2 font-bold text-white text-nowrap absolute bottom-4 text-[10px]">AWP | Neo Noir</p>
             </div>
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -3,57 +3,57 @@ import Card from "./components/card";
 
 export default function Home() {
   const cards = Array.from({ length: 21 }, (_, index) => (
-    <Card key={index} title={`AWP | Neo Noir ${index + 1}`} image="/images/awp.png" />
+    <Card key={index} title={`AWP | Neo Noir ${index + 1}`} />
   ));
   return (
     <>
     <Image src="/images/blob1.png" alt="Background" width={452} height={152} className="absolute -z-10" />
     <Image src="/images/blob2.png" alt="Background" width={552} height={152} className="absolute right-0 -z-10" />
-    <main className="home flex flex-col h-full p-4">
+    <main className="home flex flex-col h-full p-4 lg:mx-[5rem]">
       <nav className="nav flex justify-between w-full p-4 text-white">
         <div className="logo items-center flex">
           <Image src="/images/logo.png" alt="Logo" width={200} height={50} />
         </div>
         <ul className="nav-links flex space-x-4 items-center">
           <li className="nav-link text-[#3FFE9ECC] cursor-pointer hover:text-[#3FFE9ECC] transition-all font-bold">SELL SKINS</li>
-          <li className="nav-link text-[#FFFFFF4D] cursor-pointer hover:text-[#3FFE9ECC] transition-all font-bold">HOW IT WORKS?</li>
+          <li className="nav-link text-[#FFFFFF4D] cursor-pointer hover:text-[#3FFE9ECC] transition-all font-bold">HOW IT WORKS ?</li>
           <li className="nav-link text-[#FFFFFF4D] cursor-pointer hover:text-[#3FFE9ECC] transition-all font-bold">FAQ</li>
-          <button className="bg-[#3FFE9E] rounded-sm text-black font-bold p-2 hover:scale-[1.025] transition-all">
+          <button className="bg-[#3FFE9E] border-[#00000045] border-b-[3px] border-r-[3px] rounded-sm text-black font-bold p-2 hover:scale-[1.025] transition-all">
             Login via Steam
           </button>
         </ul>
       </nav>
-      <div className="flex flex-col lg:flex-row lg:space-x-4 place-content-center mt-[40px]">
-        <div className="bg-[#2B2B2B40] p-2" id="inventorypanel">
+      <div className="flex flex-col lg:flex-row lg:space-x-[3rem] place-content-center mt-[40px]">
+        <div className="bg-[#2B2B2B40] p-2 px-4" id="inventorypanel">
           <p className="text-[#FFFFFF4D]">Your Inventory</p>
           <div className="flex flex-col lg:flex-row lg:space-x-4 mt-4 lg:justify-between space-y-4 lg:space-y-0">
-            <input type="text" placeholder="Search Item..." className="bg-[#1B1B1B] border-[#222222] border-[1px] text-[#4A4A4A] placeholder-[#4A4A4A] lg:max-w-[336px]" />
-            <select name="items" id="items" className="bg-[#1B1B1B] border-[#222222] border-[1px] text-[#4A4A4A] placeholder-[#4A4A4A] w-full lg:max-w-[178px]">
+            <input type="text" placeholder="Search Item..." className="bg-[#1B1B1B] border-[#222222] border-[1px] text-[#4A4A4A] placeholder-[#4A4A4A] w-full xl:max-w-[336px] max-h-[48px] pl-4" />
+            <select name="items" id="items" className="bg-[#1B1B1B] border-[#222222] border-[1px] text-[#4A4A4A] placeholder-[#4A4A4A] max-h-[48px] lg:max-w-[178px]">
               <option value="volvo">Tradeable Items</option>
               <option value="saab">Locked Items</option>
             </select>
-            <select name="items" id="items" className="bg-[#1B1B1B] border-[#222222] border-[1px] text-[#4A4A4A] placeholder-[#4A4A4A] w-full lg:max-w-[178px]">
+            <select name="items" id="items" className="bg-[#1B1B1B] border-[#222222] border-[1px] text-[#4A4A4A] placeholder-[#4A4A4A] lg:max-w-[178px] max-h-[48px] lg:w-[178px]">
               <option value="volvo">Quality</option>
               <option value="saab">Factory New</option>
             </select>
-            <select name="items" id="items" className="bg-[#1B1B1B] border-[#222222] border-[1px] text-[#4A4A4A] placeholder-[#4A4A4A] w-full lg:max-w-[178px]">
+            <select name="items" id="items" className="bg-[#1B1B1B] border-[#222222] border-[1px] text-[#4A4A4A] placeholder-[#4A4A4A] lg:max-w-[178px] px-2 max-h-[48px] lg:w-[178px]">
               <option value="volvo">Float</option>
               <option value="saab">0-0.08</option>
             </select>
-            <div className="bg-[#1B1B1B] border-[#222222] border-[1px] text-[#4A4A4A] placeholder-[#4A4A4A] p-2 w-full max-w-[143px]">
-            <input type="checkbox" name="select-all" id="select-all" className="accent-green-400" />
+            <div className="bg-[#1B1B1B] border-[#222222] border-[1px] text-[#4A4A4A] placeholder-[#4A4A4A] p-2 w-full lg:max-w-[143px] max-h-[48px]">
+            <input type="checkbox" name="select-all" id="select-all" className="accent-[#3FFE9E]" />
             <label htmlFor="select-all" className="ml-8">Select All</label>
             </div>
           </div>
-          <div className="cards-container flex flex-wrap gap-4 mt-[40px] bg-[#1B1B1B] p-1">
+          <div className="cards-container flex flex-wrap gap-4 mt-[40px] bg-[#1B1B1B] p-2 pb-4">
             {cards}
           </div>
-          <p className="text-right text-[#3EFF9EBF] mt-4 cursor-pointer">Change trade URL</p>
+          <p className="text-right text-[#3EFF9EBF] mt-4 cursor-pointer text-[12px]">Change trade URL</p>
         </div>
 
         <div className="flex flex-col bg-[#2B2B2B40] p-2" id="payment">
           <p className="text-[#636665] font-semibold">Choose payment method</p>
-          <div className="flex w-full bg-[#1B1B1B] justify-around">
+          <div className="flex w-full bg-[#1B1B1B] justify-around mt-2 border-[1px] border-[#222222]">
             <div className="content-center p-4 max-w-[164px] max-h-[164px] cursor-pointer">
               <Image src="/images/ltc.png" alt="Paypal" width={32} height={64} className="hover:scale-110 transition-all" />
             </div>
@@ -68,41 +68,41 @@ export default function Home() {
             </div>
           </div>
           <div className="w-full justify-around flex font-bold">
-            <p className="select-none text-[#1B1B1B]">LTC</p>
-            <p className="select-none text-[#1B1B1B]">ETH</p>
+            <p className="select-none text-[#1B1B1B] opacity-0">LTC</p>
+            <p className="select-none text-[#1B1B1B] opacity-0">ETH</p>
             <p className="text-center text-[#0E9599] ml-3 text-sm">TETHER</p>
-            <p className="select-none text-[#1B1B1B]">BTC</p>
+            <p className="select-none text-[#1B1B1B] opacity-0">BTC</p>
           </div>
           <p className="text-[#636665] font-semibold">Wallet address</p>
-          <input type="text" placeholder="Enter wallet address" className="bg-[#1B1B1B] border-[#222222] border-[1px] text-[#4A4A4A] placeholder-[#4A4A4A] p-2" />
-          <p className="text-[#636665] font-semibold">Promo Code</p>
-          <div className="flex">
+          <input type="text" placeholder="Enter wallet address" className="bg-[#1B1B1B] border-[#222222] border-[1px] text-[#4A4A4A] placeholder-[#4A4A4A] p-2 mt-2" />
+          <p className="text-[#636665] font-semibold mt-4">Promo Code</p>
+          <div className="flex my-4">
             <input type="text" placeholder="Enter promo code" className="bg-[#1B1B1B] border-[#222222] border-[1px] text-[#4A4A4A] placeholder-[#4A4A4A] p-2" />
             <button className="bg-[#1F1F1F] text-[#4A4A4A] p-2">Claim</button>
           </div>
-          <p className="text-[#636665] font-semibold">Selected Items</p>
-          <div className="w-[336px] h-[234px] border-[#222222] border-[1px]"></div>
+          <p className="text-[#636665] font-medium">Selected Items</p>
+          <div className="w-[336px] h-[234px] border-[#222222] border-[1px] bg-[#1B1B1B]"></div>
           <div className="border-[#222222] border-[1px] flex flex-col p-2 mt-8 space-y-2">
             <div className="flex w-full justify-between">
-            <div className="flex">
+            <div className="flex font-medium">
               <p className="text-[#7B7B7B]">0/1000</p>
               <p className="text-[#606261] ml-2">Items selected:</p>
             </div>
-            <p className="text-[#3FFE9E]">$3,000.00</p>
+            <p className="text-[#3FFE9E] font-medium">$3,000.00</p>
             </div>
-            <div className="flex w-full justify-between">
+            <div className="flex w-full justify-between font-medium">
               <p className="text-[#606261] ">Promocode Bonus:</p>
               <p className="text-[#3FFE9E]">$300.00</p>
             </div>
-            <div className="flex w-full justify-between">
+            <div className="flex w-full justify-between font-medium">
               <p className="text-[#606261] ">Loyalty:</p>
               <p className="text-[#3FFE9E]">$55.00</p>
             </div>
-            <div className="flex w-full justify-between">
+            <div className="flex w-full justify-between font-medium">
               <p className="text-[#606261] ">Payout fee:</p>
               <p className="text-[#3FFE9E]">$120.00</p>
             </div>
-            <div className="flex">
+            <div className="flex font-medium">
             <p className="text-[#606261] font-semibold ">You will receive:</p>
             <p className="text-[#3FFE9E] ml-1 font-semibold"> $4,200.69</p>
             </div>
@@ -111,7 +111,7 @@ export default function Home() {
         </div>
       </div>
     </main>
-    <footer className="footer flex justify-between w-full p-4 text-white bg-[#1A1A1A] pt-[20px]">
+    <footer className="footer flex justify-between w-full p-4 text-white bg-[#1A1A1A] pt-[20px] lg:px-[6rem]">
         <div className="flex flex-col space-y-4">
         <div className="logo items-center flex">
           <Image src="/images/logogray.png" alt="Logo" width={150} height={50} />
@@ -126,10 +126,10 @@ export default function Home() {
             <Image src="/images/steam.png" alt="Instagram" width={32} height={32} />
           </div>
         </div>
-        <p className="text-[#393939]">2024 SkinCash. All rights </p>
-        <p className="text-[#393939]">Terms of Service</p>
+        <p className="text-[#393939] max-w-[277px] text-left">2024 SkinCash. All rights 
+        reserved. Look our Terms of Service</p>
         </div>
-        <ul className="nav-links flex space-x-4 items-center">
+        <ul className="nav-links flex lg:space-x-[10rem]  font-bold">
           <li className="nav-link text-[#8D8D8D] cursor-pointer hover:text-[#3FFE9ECC] transition-all">Product</li>
           <li className="nav-link text-[#8D8D8D] cursor-pointer hover:text-[#3FFE9ECC] transition-all">Service</li>
         </ul>
