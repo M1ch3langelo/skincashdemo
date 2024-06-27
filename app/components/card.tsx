2138d3e8a70d4e0ad1dbcc56107fbc2453180a67
Michelangelo <michelangelo.garces@hotmail.com>
2024-06-27 05:28:02 -0500

    work

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -5,18 +5,20 @@ import Image from "next/image";
 
 export default function HomeCardFN({ title }: { title: string,}) {
     return (
-        <div className="relative from-[#D72142] to-transparent border-b-2 border-b-[#D72142] shadow-md p-4 scroll-ml-6 max-w-[150.45px] card cursor-pointer hover:scale-105 transition-all h-[212px] place-content-center">
-            <Image src="/images/cardimage.png" alt="image" width={120} height={120} className="absolute top-5" />
-            <Image src="/images/cardtopright.svg" alt="topright" width={10.24} height={15.36} className="absolute top-0 right-0 z-10" />
-            <Image src="/images/cardbackground.png" alt="Lines" layout="fill" objectFit="cover" quality={100} className="absolute z-30" />
-            <div className="flex flex-col items-center">
-                <div className="flex absolute top-2 left-1 items-center">
+        <div className="relative w-[150.45px] card cursor-pointer h-[212px] place-content-center">
+            <div className="flex flex-col items-center ">
+                <div className="flex absolute top-2 left-1 items-center z-30">
                     <p className="bg-[#2D2D2D4D] rounded-full text-[#D72142CC] font-bold p-1 text-[10px]">FN</p>
                     <div className="ml-2 flex text-white font-bold text-[11px]">50.89<p className="text-[#3FFE9E]">$</p></div>
                 </div>
-                <Image src="/images/awp.png" width={132.67} height={110.18} alt="alt" />
-                <p className="mt-2 font-bold text-white text-nowrap absolute bottom-4 text-[10px]">AWP | Neo Noir</p>
+                <Image src="/images/awp.png" width={132.67} height={110.18} alt="alt" className="z-30 hover:scale-105 transition-transform" />
+                <p className="mt-2 font-bold text-white text-nowrap absolute bottom-4 text-[10px] z-30">AWP | Neo Noir</p>
             </div>
+            <Image src="/images/cardbg.svg" width={150.45} height={212} alt="cardborder" className="absolute top-0" />
+            <Image src="/images/cardimage.png" alt="image" width={130.62} height={184.11} className="absolute top-5" />
+            <Image src="/images/cardtopright.svg" alt="topright" width={5.24} height={5.36} className="absolute top-0 right-0 z-10" />
+            <div className="bglines z-20 absolute h-[35%] w-full bottom-0" />
+            <div className="bggradient z-10 absolute h-[80%] w-full bottom-0 border-b-[#D72142] border-b-[1px]" />
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -84,54 +84,55 @@ export default function Home() {
             <label htmlFor="select-all" className="ml-8">Select All</label>
             </div>
           </div>
-          <div className="cards-container flex flex-wrap gap-4 mt-[40px] bg-[#1B1B1B] p-2 pb-4">
+          <div className="cards-container scrollbar overflow-y-scroll overflow-x-hidden h-[687px] grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 gap-2 mt-[40px] bg-[#1B1B1B] p-2 pb-4" id="cards-container">
             {cards}
           </div>
           <p className="text-right text-[#3EFF9EBF] mt-4 cursor-pointer text-[12px]">Change trade URL</p>
         </div>
 
-        <div className="mt-4 lg:mt-0 flex flex-col bg-[#2B2B2B40] p-2" id="payment">
+        <div className="mt-4 lg:mt-0 flex flex-col bg-[#2B2B2B40] p-2 lg:max-w-[368px]" id="payment">
           <p className="text-[#636665] font-semibold">Choose payment method</p>
-          <div className="flex w-full bg-[#1B1B1B] justify-evenly mt-2 border-[1px] border-[#222222]">
+          <div className="w-full bg-[#1B1B1B] grid grid-cols-4 h-[84px] mt-2 border-[1px] border-[#222222]">
             <div 
-              className={`content-center p-4 max-w-[164px] max-h-[164px] cursor-pointer ${isLtcSelected ? 'ltcselected' : ''}`} 
+              className={`p-4 max-w-[164px] max-h-[164px] cursor-pointer flex items-center ${isLtcSelected ? 'border-[1px] border-[#898989]' : ''}`} 
               onClick={handleLtcSelected}>
-              <svg width="28" height="32" viewBox="0 0 28 32" fill="none" xmlns="http://www.w3.org/2000/svg" className={`ltc ${isLtcSelected ? 'fill-[#898989]' : ''}`}>
+              <svg width="58" height="42" viewBox="0 0 28 32" fill="none" xmlns="http://www.w3.org/2000/svg" className={`${isLtcSelected ? 'ltcselected' : ''}`}>
                 <path fillRule="evenodd" clipRule="evenodd" d="M2.82682 22.428L0 23.536L1.36289 18.018L4.22339 16.858L8.34575 0H18.5061L15.497 12.392L18.2901 11.25L16.9431 16.75L14.1163 17.892L12.4364 24.858H27.7333L26.004 32H0.4992L2.82682 22.428Z" fill={isLtcSelected ? "#898989" : "#3C3C3C"}/>
               </svg>
             </div>
             <div 
-              className={`content-center p-4 max-w-[164px] max-h-[164px] cursor-pointer ${isEthSelected ? 'ethselected' : ''}`}
+              className={`flex items-center place-content-center p-4 max-w-[164px] max-h-[164px] cursor-pointer ${isEthSelected ? 'border-[1px] border-[#798EFF]' : ''}`}
               onClick={handleEthSelected}>
-              <svg width="26" height="41" viewBox="0 0 26 41" fill="none" xmlns="http://www.w3.org/2000/svg" className={`eth ${isEthSelected ? 'fill-[#48cbd9]' : ''}`}>
-                <path d="M12.8087 0V15.1524L25.6156 20.8751L12.8087 0Z" fill={isEthSelected ? "#627EEB" : "#FFFFFF26"} fillOpacity="1"/>
-                <path d="M12.8086 0L0 20.8751L12.8086 15.1524V0Z" fill={isEthSelected ? "#627EEB" : "#FFFFFF1C"} fillOpacity="1"/>
-                <path d="M12.8086 30.6942V40.9899L25.6241 23.2598L12.8086 30.6942Z" fill={isEthSelected ? "#627EEB" : "#FFFFFF26"} fillOpacity="1"/>
-                <path d="M12.8086 40.9899V30.6925L0 23.2598L12.8086 40.9899Z" fill={isEthSelected ? "#627EEB" : "#FFFFFF1A"} fillOpacity="1"/>
-                <path d="M12.8086 28.3117L25.6155 20.8755L12.8086 15.1562V28.3117Z" fill={isEthSelected ? "#627EEB" : "#FFFFFF1A"} fillOpacity="1"/>
-                <path d="M0 20.8755L12.8086 28.3117V15.1562L0 20.8755Z" fill={isEthSelected ? "#627EEB" : "#FFFFFF26"} fillOpacity="1"/>
+              <svg width="58" height="42" viewBox="0 0 26 41" fill="none" xmlns="http://www.w3.org/2000/svg" className={`${isEthSelected ? 'fill-[#798EFF] ethselected ' : ''}`}>
+                <path d="M12.8087 0V15.1524L25.6156 20.8751L12.8087 0Z" fill={isEthSelected ? "#798EFF" : "#FFFFFF26"} fillOpacity="1"/>
+                <path d="M12.8086 0L0 20.8751L12.8086 15.1524V0Z" fill={isEthSelected ? "#798EFF" : "#FFFFFF1C"} fillOpacity="1"/>
+                <path d="M12.8086 30.6942V40.9899L25.6241 23.2598L12.8086 30.6942Z" fill={isEthSelected ? "#798EFF" : "#FFFFFF26"} fillOpacity="1"/>
+                <path d="M12.8086 40.9899V30.6925L0 23.2598L12.8086 40.9899Z" fill={isEthSelected ? "#798EFF" : "#FFFFFF1A"} fillOpacity="1"/>
+                <path d="M12.8086 28.3117L25.6155 20.8755L12.8086 15.1562V28.3117Z" fill={isEthSelected ? "#798EFF" : "#FFFFFF1A"} fillOpacity="1"/>
+                <path d="M0 20.8755L12.8086 28.3117V15.1562L0 20.8755Z" fill={isEthSelected ? "#798EFF" : "#FFFFFF26"} fillOpacity="1"/>
               </svg>
             </div>
             <div 
-              className={`content-center max-w-[164px] max-h-[164px] cursor-pointer ${isUsdtSelected ? 'coinselected' : ''}`}
+              className={` ${isUsdtSelected ? 'border-[1px] border-[#0E959980]' : ''} flex items-center place-content-center max-w-[164px] max-h-[164px] cursor-pointer`}
               onClick={handleUsdtSelected}>
-              <svg width="97" height="38" viewBox="0 0 797 738" fill="none" xmlns="http://www.w3.org/2000/svg" className={`coin ${isUsdtSelected ? 'fill-[#039196]' : ''}`}>
-                <path d="M797.374 348.792C797.374 298.54 660.637 256.613 478.929 246.94L478.915 166.872H705.799V0.237793H85.404V166.872H312.27V247.332C133.871 257.668 0.421875 299.157 0.421875 348.792C0.421875 398.433 133.871 439.926 312.27 450.245V737.766H478.915V450.636C660.637 440.984 797.374 399.044 797.374 348.792ZM398.896 398.296C203.857 398.296 45.7422 368.109 45.7422 330.85C45.7422 299.302 159.119 272.816 312.27 265.446V284.436H312.288V362.253C339.899 363.802 368.918 364.613 398.896 364.613C426.5 364.613 453.307 363.911 478.932 362.627V265.157C635.403 272.075 752.057 298.839 752.057 330.85C752.054 368.109 593.935 398.296 398.896 398.296Z" fill={isUsdtSelected ? "#039196" : "#3C3C3C"}/>
+              <svg width="58" height="42" viewBox="0 0 797 738" fill="none" xmlns="http://www.w3.org/2000/svg" className={`${isUsdtSelected ? 'usdtselected' : ''}`}>
+                <path d="M797.374 348.792C797.374 298.54 660.637 256.613 478.929 246.94L478.915 166.872H705.799V0.237793H85.404V166.872H312.27V247.332C133.871 257.668 0.421875 299.157 0.421875 348.792C0.421875 398.433 133.871 439.926 312.27 450.245V737.766H478.915V450.636C660.637 440.984 797.374 399.044 797.374 348.792ZM398.896 398.296C203.857 398.296 45.7422 368.109 45.7422 330.85C45.7422 299.302 159.119 272.816 312.27 265.446V284.436H312.288V362.253C339.899 363.802 368.918 364.613 398.896 364.613C426.5 364.613 453.307 363.911 478.932 362.627V265.157C635.403 272.075 752.057 298.839 752.057 330.85C752.054 368.109 593.935 398.296 398.896 398.296Z" fill={isUsdtSelected ? "#039196" : "#3C3C3C"} 
+                />
               </svg>
             </div>
             <div 
               onClick={handleBtcSelected}
-              className={`content-center max-w-[164px] max-h-[164px] cursor-pointer ${isBtcSelected ? 'btcselected' : ''}`}>
-              <svg width="31" height="40" viewBox="0 0 31 40" fill="none" xmlns="http://www.w3.org/2000/svg" className={`btc ${isBtcSelected ? 'fill-[#039196]' : ''}`}>
+              className={`flex items-center place-content-center max-w-[164px] max-h-[164px] cursor-pointer ${isBtcSelected ? 'border-[1px] border-[#FFAC00]' : ''}`}>
+              <svg width="58" height="42" viewBox="0 0 31 40" fill="none" xmlns="http://www.w3.org/2000/svg" className={`${isBtcSelected ? 'btcselected' : ''}`}>
                 <path d="M30.3765 16.04C31.0045 11.848 27.8105 9.594 23.4465 8.09L24.8625 2.41L21.4065 1.55L20.0265 7.08C19.1185 6.852 18.1865 6.64 17.2565 6.428L18.6465 0.862L15.1905 0L13.7745 5.678C13.0225 5.506 12.2825 5.338 11.5665 5.158L11.5705 5.14L6.8025 3.95L5.8825 7.642C5.8825 7.642 8.4485 8.23 8.3945 8.266C9.7945 8.616 10.0465 9.542 10.0045 10.278L8.3925 16.748C8.4885 16.772 8.6125 16.808 8.7525 16.862L8.3865 16.772L6.1265 25.836C5.9545 26.26 5.5205 26.898 4.5405 26.656C4.5765 26.706 2.0285 26.03 2.0285 26.03L0.3125 29.986L4.8125 31.108C5.6485 31.318 6.4685 31.538 7.2745 31.744L5.8445 37.488L9.2985 38.348L10.7145 32.668C11.6585 32.922 12.5745 33.158 13.4705 33.382L12.0585 39.038L15.5145 39.898L16.9445 34.166C22.8405 35.282 27.2725 34.832 29.1385 29.5C30.6425 25.208 29.0645 22.73 25.9625 21.116C28.2225 20.596 29.9225 19.11 30.3765 16.04ZM22.4765 27.116C21.4105 31.41 14.1805 29.088 11.8365 28.506L13.7365 20.896C16.0805 21.482 23.5945 22.64 22.4765 27.116ZM23.5465 15.978C22.5725 19.884 16.5565 17.898 14.6065 17.412L16.3265 10.512C18.2765 10.998 24.5625 11.904 23.5465 15.978Z" fill={isBtcSelected ? "#FFAC00" : "#3C3C3C"}/>
               </svg>
             </div>
           </div>
-          <div className="w-full justify-evenly space-x-7 flex font-bold">
-          <p className={`select-none pl-4 ${isLtcSelected ? 'text-[#898989]' : 'opacity-0'}`}>LTC</p>
-            <p className={`select-none pl-2 ${isEthSelected ? 'text-[#627EEB]' : 'opacity-0'}`}>ETH</p>
-            <p className={`select-none pl-4 ${isUsdtSelected ? 'text-[#0E9599]' : 'opacity-0'}`}>USDT</p>
-            <p className={`select-none ${isBtcSelected ? 'text-[#FFAC00]' : 'opacity-0'}`}>BTC</p>
+          <div className="w-full grid grid-cols-4 font-bold mt-2">
+            <p className={`select-none justify-center flex ${isLtcSelected ? 'text-[#898989] animate-text-up' : 'opacity-0'}`}>LTC</p>
+            <p className={`select-none justify-center flex ${isEthSelected ? 'text-[#627EEB] animate-text-up' : 'opacity-0'}`}>ETH</p>
+            <p className={`select-none justify-center flex ${isUsdtSelected ? 'text-[#0E9599] animate-text-up' : 'opacity-0'}`}>USDT</p>
+            <p className={`select-none justify-center flex ${isBtcSelected ? 'text-[#FFAC00] animate-text-up' : 'opacity-0'}`}>BTC</p>
           </div>
           <p className="text-[#636665] font-semibold">Wallet address</p>
           <input type="text" placeholder="Enter wallet address" className="bg-[#1B1B1B] border-[#222222] border-[1px] text-[#4A4A4A] placeholder-[#4A4A4A] p-2 mt-2" />
